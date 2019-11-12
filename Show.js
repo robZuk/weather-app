fdcc571fca7939f94be5354d552467224fe940b7
Robert Żuk <robert.aleks.zuk@gmail.com>
2019-11-12 07:24:22 +0100

    add current weather

--- Show.js
+++ Show.js
@@ -21,28 +21,32 @@ class Show {
       this.listForecastWeather.classList.remove("active");
       this.btnForecastWeather.classList.remove("active");
     }
-    const sunrise = new Date(result.city.sunrise)
+    const sunrise = new Date(result.city.sunrise * 1000)
       .toLocaleTimeString()
       .substring(0, 5); //*1000 pozbywamy się milisekund
-    const sunset = new Date(result.city.sunset)
+    const sunset = new Date(result.city.sunset * 1000)
       .toLocaleTimeString()
       .substring(0, 5);
+    const date = new Date().toDateString();
     const temperature = (result.list[0].main.temp - 273.15).toFixed(0);
     this.resultAreaCurrentWeather.textContent = "";
     const content = `
-
+    
     <div class="current_weather__list_header">
     <div class="current_weather__list_header_city">Weather in ${result.city.name}, ${result.city.country}</div>
     <img class="current_weather__list_header_img" src="http://openweathermap.org/img/wn/${result.list[0].weather[0].icon}.png">
     <div class="current_weather__list_header_temp">${temperature} &#176;C</div>
     <div class="current_weather__list_header_description">${result.list[0].weather[0].description}</div>
+    <div class="current_weather__list_header_date">${date}</div>
     </div>
+    <div class="current_weather__list_main">
     <div class="current_weather__list_sunrise">Sunrise: ${sunrise}</div>
     <div class="current_weather__list_sunset">Sunset: ${sunset}</div>
     <div class="current_weather__list_pressure">Pressure: ${result.list[0].main.pressure} hpa</div>
     <div class="current_weather__list_header_humidity">Humidity: ${result.list[0].main.humidity} %</div>
     <div class="current_weather__list_header_cloud">Clouds: ${result.list[0].clouds.all} %</div>
     <div class="current_weather__list_header_wind">Wind: ${result.list[0].wind.speed} m/s</div>
+    </div>
     `;
 
     this.resultAreaCurrentWeather.innerHTML = content;
